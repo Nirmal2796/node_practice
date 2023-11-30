e21b7f04dc7cafce47adca7e3211205046528c3f
Nirmal2796 <nirmalgadekar2796@gmail.com>
2023-11-30 16:38:38 +0530

    Return a Response

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Nirmal');
+   
+    if (req.url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (req.url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>About us</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (req.url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node Js Project</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    res.setHeader('content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my node js server </h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000);
