f118379818b4b0ac5cafd1928e113d19c82f0ecb
Nirmal2796 <nirmalgadekar2796@gmail.com>
2023-12-01 17:29:44 +0530

    Writing in a file and redirecting

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
    
+    if (req.url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunks) => {
+            body.push(chunks);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+           
+
+        })
+        
+        
+    }
+
+
+
+
     if (req.url === '/home') {
         res.write('<html>');
         res.write('<head><title>Home</title></head>');
